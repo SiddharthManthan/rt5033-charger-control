05773399a558d79692b0bf23166a1fa9d2e64643
Siddharth Manthan <siddharth.manthan@gmail.com>
2022-03-08 19:39:04 +0530

    toggle-charger: try: Disable Timer

--- toggle-charger.sh
+++ toggle-charger.sh
@@ -46,12 +46,30 @@ test "$1" != "on" && test "$1" != "off" && bailout
 if test "$1" == "on"; then {
 	# # Disable high Impedance Mode
 	i2c_assign_bits $bus $address $RT5033_CHG_CTRL1 0x02 0x00
+	
+	# # Set COF_EN_MASK
+	# # Verify if this is really necessary
+	# # i2c_assign_bits $bus $address $RT5033_CHG_CTRL3 $RT5033_COF_EN_MASK 0x01
+
+	# # Disable Internal Timer. Maybe max amount of time charger will be enabled
+	# # Verify if this is really necessary
+	# i2c_assign_bits $bus $address $RT5033_CHG_CTRL3 $RT5033_TIMEREN_MASK 0x00
+	
+	# # Reset EOC loop, and make it re-detect
+	# # Set EOC_RESET_MASK
+	# i2c_assign_bits $bus $address $RT5033_CHG_STAT_CTRL $RT5033_CHGENB_MASK 0x00
+	# # Reset EOC_RESET_MASK
+	# i2c_assign_bits $bus $address $RT5033_EOC_CTRL $RT5033_EOC_RESET_MASK 0x01
+	# i2c_assign_bits $bus $address $RT5033_EOC_CTRL $RT5033_EOC_RESET_MASK 0x00
 
 	# Enable Charger
 	i2c_assign_bits $bus $address $RT5033_CHG_STAT_CTRL 0x01 0x00
 
 	logger "turned charger on"
 } else {
+	# # Disable EOC
+	# i2c_assign_bits $bus $address $RT5033_CHG_CTRL4 $RT5033_IEOC_MASK 0x00
+
 	# Disable Charger
 	i2c_assign_bits $bus $address $RT5033_CHG_STAT_CTRL 0x01 0x01
 
